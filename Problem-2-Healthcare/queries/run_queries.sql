d71968e87f80c2faaa6fa94c5871aa62aedd9432
mikebrownw <mikebrownw@outlook.com>
2026-02-25 09:56:43 -0500

    Update and rename placeholder.txt to run_queries.sql

---
+++ Problem-2-Healthcare/queries/run_queries.sql
@@ -0,0 +1,16 @@
+-- Test Question 1
+SELECT * FROM patients_with_no_recent_appts;
+
+-- Test Question 2
+EXEC find_patients_with_consecutive_no_shows @min_streak = 3;
+
+-- Test Question 3
+EXEC get_upcoming_appointments 
+    @region = 'Northeast', 
+    @from_date = '2025-07-01', 
+    @to_date = '2025-12-31';
+
+-- Test Question 4
+SELECT * FROM vw_monthly_no_show_stats 
+WHERE year = 2025 
+ORDER BY month, region;
