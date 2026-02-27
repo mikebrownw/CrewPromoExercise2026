57240a1651925b1a37c845e6b9ecaab1944733d0
mikebrownw <mikebrownw@outlook.com>
2026-02-27 09:25:41 -0500

    Update 00_test_queries.sql

--- Problem-1-Retail/queries/00_test_queries.sql
+++ Problem-1-Retail/queries/00_test_queries.sql
@@ -9,16 +9,16 @@ ORDER BY order_id ASC;
 SELECT * FROM order_items
 ORDER BY order_id ASC;
 
--- View all customers
-SELECT 'customers' AS table_name, * FROM customers;
+-------------------------------------------------------------
 
--- View all products
-SELECT 'products' AS table_name, * FROM products;
+PRINT '========== CUSTOMERS ==========';
+SELECT * FROM customers;
 
--- View all orders (sorted)
-SELECT 'orders' AS table_name, * FROM orders
-ORDER BY order_id ASC;
+PRINT '========== PRODUCTS ==========';
+SELECT * FROM products;
 
--- View all order_items (sorted)
-SELECT 'order_items' AS table_name, * FROM order_items
-ORDER BY order_id ASC;
+PRINT '========== ORDERS ==========';
+SELECT * FROM orders ORDER BY order_id;
+
+PRINT '========== ORDER ITEMS ==========';
+SELECT * FROM order_items ORDER BY order_id;
