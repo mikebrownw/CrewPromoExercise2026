2b41f0ee12f68f198d3d3d895859c2825b8a1b8e
mikebrownw <mikebrownw@outlook.com>
2026-03-02 09:10:40 -0500

    Update 01_create_tables.sql

--- Problem-5-HR-Temporal/data-model/01_create_tables.sql
+++ Problem-5-HR-Temporal/data-model/01_create_tables.sql
@@ -1,6 +1,10 @@
 -- SQL Server (T-SQL)
 -- Complete Data Model for HR/Temporal/Security
 
+IF OBJECT_ID('users', 'U') IS NOT NULL DROP TABLE users;
+IF OBJECT_ID('comp_changes', 'U') IS NOT NULL DROP TABLE comp_changes;
+IF OBJECT_ID('employees', 'U') IS NOT NULL DROP TABLE employees;
+
 -- Create employees table with self-referencing hierarchy
 CREATE TABLE employees (
     emp_id INT PRIMARY KEY,
